819848cbee54cb95813721bb616a785b0a4757b0
vainqueur <vainqueurmg@gmail.com>
2025-05-30 11:36:35 +0200

    feat: add image upload functionality with Cloudinary and Multer

---
+++ src/middlewares/multerUpload.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+
+const uploadImage = multer({
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        let ext = path.extname(file.originalname);
+        if (
+            ext !== ".png" &&
+            ext !== ".jpg" &&
+            ext !== ".jpeg" &&
+            ext !== ".gif" &&
+            ext !== ".tif"
+        ) {
+            return cb(new Error("Invalid image format, try (png, jpg, gif, tif or jpeg) !"), false);
+        }
+        cb(null, true);
+    },
+});
+
+export default uploadImage;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,10 +3,11 @@ import auth from "../controllers/authController";
 import { bodyValidation } from "../middlewares/bodyValidation";
 import { loginSchema } from "../validation/authValidation";
 import { newUserSchema } from "../validation/userValidation";
+import uploadImage from "../middlewares/multerUpload";
 
 const authRouter = express.Router()
 
-authRouter.post("/SignUp", bodyValidation(newUserSchema), auth.createUser)
-authRouter.post("/login", bodyValidation(loginSchema) , auth.userLogin)
+authRouter.post("/SignUp", uploadImage.single("profileImage"), bodyValidation(newUserSchema), auth.createUser)
+authRouter.post("/login", bodyValidation(loginSchema), auth.userLogin)
 
 export default authRouter;
